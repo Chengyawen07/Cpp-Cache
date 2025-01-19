a1839e6cc77673d3039b375f6aaae8fbb0881aa4
Chengyawen07 <yawencheng.cs@gmail.com>
2025-01-19 22:00:00 +0800

    cache

--- LruCache.h
+++ LruCache.h
@@ -7,6 +7,8 @@
 // make_shared()
 #include <memory>
 #include <unordered_map>
+#include <mutex>
+#include <thread>
 using namespace std;
 
 
@@ -52,32 +54,82 @@ class LruCache
 public:
 
     // 用using取别名，简化后面的代码书写
-    using LruNodeType = LruNode(Key, Value);
+    // 这里的NodeType就是一个链表节点Node，包含Key 和 Value
+    using LruNodeType = LruNode<Key, Value>;
+    // 这个是指向链表的指针
+    using NodePtr = std::shared_ptr<LruNodeType>;
+    // 定义一个关联容器unordered map，里面有Key和Value，Value是一个链表指针类型
+    using NodeMap = std::unordered_map<Key, NodePtr>;
 
 
     // 1. 构造函数
     LruCache(int capacity) : capacity_(capacity) {
+        // 用来创建虚拟链表
         initializeList();
     }
 
     ~LruCache() override = default;
 
 
-    void initializeList(){
-        // 创建首尾的虚拟节点
-        dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
-        dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+    // 2. 业务逻辑：插入数据
+    void put(Key key, Value value){
+        // 如果缓存为0，直接返回，不执行操作
+        if(capacity_ <= 0) return;
+
+        // 使用 std::lock_guard，自动加锁，代替了手动加锁
+        std::lock_guard<std::mutex> lock(mutex_);
+        // 在哈希表中找key
+        auto it = nodeMap_.find(key);
+
+        // 如果找到了key，就更新节点，并将节点移动到链表头部，标记为最近使用
+        if(it != nodeMap_.end()){
+            updateExistingNode(it->second, value);
+            return;
+        }
+        
+    }
+
+    // 3. 获取数据
+    bool get(Key key, Value value){
+        // 1. 加锁保护共享资源（如 nodeMap_ 和链表）不被多个线程同时修改
+        std::lock_guard<std::mutex> lock(mutex_);
+        // 在哈希表 nodeMap_ 中查找键 key 是否存在，返回迭代器it
+        auto it = nodeMap_.find(key);
+        // 如果找到了key
+        if (it != nodeMap_.end()) {
+            moveToMostRecent(it->second); // 将节点移动到链表头部
+            value = it->second->getValue(); // 通过引用参数返回值
+            return true; // 返回成功
+        }
 
 
     }
 
 
+
+    // 4. 删除数据
+    void remove(){
+
+    }
+
+
+
+private:
+    void initializeList(){
+        // 创建首尾的虚拟节点
+        dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
+        dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+        dummyHead_->next_ = dummyTail_;
+        dummyTail_->prev_ = dummyHead_;
+    }
+
+
 private:
     int capacity_;
-    std:shared_ptr<LruNodeType> dummyHead_;
-    std:shared_ptr<LruNodeType> dummyTail_;
-
-
+    std::shared_ptr<LruNodeType> dummyHead_;
+    std::shared_ptr<LruNodeType> dummyTail_;
+    std::mutex  mutex_;
+    NodeMap nodeMap_; 
 };
 
 
